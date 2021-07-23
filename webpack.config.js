56bfa0519d04857d490d890ba14f3e93d7e15821
kstar0101 <canopus0410@gmail.com>
2021-07-23 10:04:34 +0900

    init vue

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ChunkRenamePlugin = require('webpack-chunk-rename-plugin');
+
+function resolve(dir) {
+  return path.join(
+    __dirname,
+    '/resources/vue/js',
+    dir
+  );
+}
+
+const rawArgv = process.argv.slice(2);
+const report = rawArgv.includes('--report');
+const plugins = [new ChunkRenamePlugin({ initialChunksWithEntry: true, '/assets/js/app': 'assets/js/app.js', '/assets/js/vendor': 'assets/js/vendor.js' })];
+if (report) {
+  plugins.push(new BundleAnalyzerPlugin({
+    openAnalyzer: true,
+  }));
+}
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      '@': path.join(__dirname, '/resources/vue/js'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('icons')],
+        options: {
+          symbolId: 'icon-[name]',
+        },
+      },
+    ],
+  },
+  plugins: plugins,
+  output: {
+    filename: '[name].js',
+    chunkFilename: 'assets/js/[name].[chunkhash:6].js',
+  },
+};
