547ec7504fcde38531d86f14f4f696583b91b709
kstar0101 <canopus0410@gmail.com>
2021-09-29 18:57:58 +0900

    sqladd

--- database/seeds/SqlFileSeeder.php
+++ database/seeds/SqlFileSeeder.php
@@ -12,7 +12,6 @@ class SqlFileSeeder extends Seeder
     public function run()
     {
         //
-
         $path = public_path('sql/zensho_maintenance.sql');
         $sql = file_get_contents($path);
         DB::unprepared($sql);
